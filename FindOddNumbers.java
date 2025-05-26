7d96f0108fcfdfb94fa98c2100f795398a6aabe3
nagarajanmurugesan <nagarajan091997@gmail.com>
2025-05-26 13:30:26 +0530

    Week 1
    Day 1 and Day 2

---
+++ FindOddNumbers.java
@@ -0,0 +1,46 @@
+package week1.day2;
+
+public class FindOddNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            if (i%2==0) {
+                continue;
+            }
+
+            else
+            {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
+
+//package week1.day2;
+//public class FindOddNumbers {
+// public static void main(String[] args) {
+//   for (int i = 1; i <= 10; i++) {
+//  if (i % 2 == 1) {
+//     System.out.println(i);
+//  }
+//  }
+//}
+
+
+
+///package week1.day2
+
+//public class FindOddNumbers {
+///public static void main(String[] args) {
+//for (int i = 1; i <=10 ; i++) {
+// if (i%2 !=0) {
+//System.out.println(i);
+//      }
+
+
+
+//   }
+// }
+
+// }
+//}
